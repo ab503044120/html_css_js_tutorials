42f07642757b4dc5152e21f0fc2347449b6c0fe4
ab503044120 <503044120@qq.com>
2017-08-25 18:38:06 +0800

    sync

--- vue/05.webpack/04.vue-elementui/src/components/pages/ANDROID.vue
+++ vue/05.webpack/04.vue-elementui/src/components/pages/ANDROID.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="android-container" v-loading="loading">
-  
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in datas" :key="index" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img :src="o.images[0]" class="image" >
+          <div style="padding: 14px;">
+            <span>{{o.desc}}</span>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -8,14 +17,22 @@
 export default {
   mounted: function () {
     this.$http('http://gank.io/api/random/data/Android/20').then(response => {
-      this.datas = response.data
+      console.log(response)
+      if (!response.data.error) {
+        var self = this
+        response.data.results.forEach(function (element) {
+          if (element.images != null) {
+            self.datas.push(element)
+          }
+        })
+      }
       this.loading = false
     })
   },
   data: () => {
     return {
       loading: true,
-      datas: {}
+      datas: []
     }
   }
 }
@@ -26,5 +43,9 @@ export default {
   height: 100%;
   width: 100%;
   position: relative
+}
+.image{
+  height: 50%;
+  width: 50%;
 }
 </style>
